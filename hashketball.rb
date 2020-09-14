85e55d434c68a8eb309cc291dfdc92c2f46f1aa2
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-14 18:42:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,7 @@ def game_hash
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => [{}]
+    :players => [:player_name =>{}]
   },
   :away => {
     :team_name => "",
